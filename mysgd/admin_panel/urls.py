ce2c51fa525676e9418b41a6eb6381e370ae76c9
christian460 <64232962+christian460@users.noreply.github.com>
2024-07-22 02:22:18 -0500

    incorporacion del apartado de proveedores y adicion de base html

--- mysgd/admin_panel/urls.py
+++ mysgd/admin_panel/urls.py
@@ -8,5 +8,6 @@ app_name = 'admin_panel'
 urlpatterns = [
     path('profiles/', views.profile_list, name='profile_list'),
     path('profiles/<int:pk>/', views.profile_detail, name='profile_detail'),
+    path('proveedores/', views.proveedores_list, name='proveedores_list'),
     # Agrega más URLs según sea necesario para los otros modelos (productos, proveedores, órdenes, etc.)
 ]
--- mysgd/admin_panel/views.py
+++ mysgd/admin_panel/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from shop.models import Profile  # Importa los modelos necesarios
+from shop.models import Profile, Supplier  # Importa los modelos necesarios
 from django.shortcuts import render, get_object_or_404
 
 def profile_list(request):
     profiles = Profile.objects.all()
     return render(request, 'profile_list.html', {'profiles': profiles})
+
+def proveedores_list(request):
+    proveedores = Supplier.objects.all()
+    return render(request, 'productores.html', {'proveedores': proveedores})
 
 def profile_detail(request, pk):
     profile = get_object_or_404(Profile, pk=pk)
